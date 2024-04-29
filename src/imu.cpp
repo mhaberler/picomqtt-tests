4261d88524354baf2902eaf9e4a24811aa90cf08
Michael Haberler <git@mah.priv.at>
2024-04-29 11:01:07 +0200

    DMP example., quat output, compass broken

--- src/imu.cpp
+++ src/imu.cpp
@@ -11,44 +11,72 @@ static ICM_20948_I2C icm;
 
 bool imu_setup( void) {
     if (detect(Wire, ICM_20948_I2C_ADDR_AD1)) {
+        bool success = true;
 
         icm.begin(Wire, ICM_20948_I2C_ADDR_AD1);
         log_i("status: %s", icm.statusString());
 
-        // icm.swReset();
-        // log_i("status: %s", icm.statusString());
+        success &= (icm.swReset() == ICM_20948_Stat_Ok);
         delay(250);
-        // icm.sleep(false);
-        // log_i("status: %s", icm.statusString());
+        success &= (icm.sleep(false) == ICM_20948_Stat_Ok);
+        success &= (icm.lowPower(false) == ICM_20948_Stat_Ok);
 
-        // icm.lowPower(false);
-        // log_i("status: %s %d", icm.statusString(), icm.status);
-        return true;
+        success &= (icm.initializeDMP() == ICM_20948_Stat_Ok);
+        success &= (icm.enableDMPSensor(INV_ICM20948_SENSOR_ORIENTATION) == ICM_20948_Stat_Ok); // 9-axis quaternion + heading accuracy
+        success &= (icm.enableDMPSensor(INV_ICM20948_SENSOR_GEOMAGNETIC_FIELD) == ICM_20948_Stat_Ok); // 32-bit calibrated compass
+        success &= (icm.setDMPODRrate(DMP_ODR_Reg_Quat9, 10) == ICM_20948_Stat_Ok); // Set to 5Hz for motion quaternion
+
+        success &= (icm.setDMPODRrate(DMP_ODR_Reg_Cpass_Calibr, 10) == ICM_20948_Stat_Ok); // Set to 5Hz
+
+        success &= (icm.enableFIFO() == ICM_20948_Stat_Ok);
+        success &= (icm.enableDMP() == ICM_20948_Stat_Ok);
+        success &= (icm.resetDMP() == ICM_20948_Stat_Ok);
+        success &= (icm.resetFIFO() == ICM_20948_Stat_Ok);
+
+        log_i("status: %s %d success=%d", icm.statusString(), icm.status, success);
+
+        return success;
         return (icm.status == ICM_20948_Stat_Ok) || (icm.status ==  ICM_20948_Stat_NoData);
     }
     return false;
 }
 
 void imu_loop( void) {
-    if (icm.dataReady())   {
-        icm.getAGMT();
-        JsonDocument json;
-        json["us"] = micros();
+    icm_20948_DMP_data_t data;
+    icm.readDMPdataFromFIFO(&data);
+    unsigned long now = micros();
 
-        json["ax"] = icm.accX();
-        json["ay"] = icm.accY();
-        json["az"] = icm.accZ();
+    if ((icm.status == ICM_20948_Stat_Ok) || (icm.status == ICM_20948_Stat_FIFOMoreDataAvail)) {
 
-        json["gx"] = icm.gyrX();
-        json["gy"] = icm.gyrY();
-        json["gz"] = icm.gyrZ();
+        if ((data.header & DMP_header_bitmap_Quat9) > 0) {
+            double q1 = ((double)data.Quat9.Data.Q1) / 1073741824.0; // Convert to double. Divide by 2^30
+            double q2 = ((double)data.Quat9.Data.Q2) / 1073741824.0; // Convert to double. Divide by 2^30
+            double q3 = ((double)data.Quat9.Data.Q3) / 1073741824.0; // Convert to double. Divide by 2^30
+            double q0 = sqrt(abs(1.0 - ((q1 * q1) + (q2 * q2) + (q3 * q3))));
 
-        json["mx"] = icm.magX();
-        json["my"] = icm.magY();
-        json["mz"] = icm.magZ();
+            JsonDocument json;
 
-        auto publish = mqtt.begin_publish("imu", measureJson(json));
-        serializeJson(json, publish);
-        publish.send();
+            json["us"] = now;
+
+            json["w"] = q0;
+            json["x"] = q1;
+            json["y"] = q2;
+            json["z"] = q3;
+
+            auto publish = mqtt.begin_publish("quat", measureJson(json));
+            serializeJson(json, publish);
+            publish.send();
+        }
+        if ((data.header & DMP_header_bitmap_Compass_Calibr) > 0)  {
+            JsonDocument json;
+            json["us"] = now;
+
+            json["mx"] = data.Compass_Calibr.Data.X;
+            json["my"] = data.Compass_Calibr.Data.Y;
+            json["mz"] = data.Compass_Calibr.Data.Z;
+            auto publish = mqtt.begin_publish("compass", measureJson(json));
+            serializeJson(json, publish);
+            publish.send();
+        }
     }
 }
