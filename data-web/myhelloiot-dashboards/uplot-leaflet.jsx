2ed4c0c486c8c5addd1bc7645adb6854df5356b0
Michael Haberler <git@mah.priv.at>
2024-06-14 23:02:54 +0200

    uplot + leaflet demo

---
+++ data-web/myhelloiot-dashboards/uplot-leaflet.jsx
@@ -0,0 +1,18 @@
+<DashboardPage title="Uplot & Leaflet demo">
+    
+    <Card title="Map">
+
+        <LeafletUnit
+            topic="gps/nav"
+        />
+    </Card>
+
+    <Card title="Time series">
+
+        <UplotUnit
+            topic="baro/baro-0"
+
+        />
+    </Card>
+
+</DashboardPage >
